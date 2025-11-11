8e9021e7fb8c5003953658a06a5bfe23efc71859
Phuoc1110 <phuoc11102004@gmail.com>
2025-11-11 22:00:15 +0700

    set up project

---
+++ src/test/java/com/group4/expense_manager/ExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group4.expense_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
