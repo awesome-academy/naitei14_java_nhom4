ff29545c8fe7129ea5a2127b95c4051a43f0ae7c
Phuoc1110 <phuoc11102004@gmail.com>
2025-11-23 11:17:37 +0700

    test

---
+++ src/main/java/com/group4/expense_manager/service/DashboardService.java
@@ -0,0 +1,11 @@
+package com.group4.expense_manager.service;
+
+import com.group4.expense_manager.entity.Expense;
+import com.group4.expense_manager.entity.Income;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface DashboardService {
+
+}
