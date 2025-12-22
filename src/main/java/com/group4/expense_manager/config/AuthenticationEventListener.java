20cfffba3f3089539c281ab1395ea6faa0fd3e1c
toan <toanle1355@gmail.com>
2025-12-22 21:15:57 +0700

    fix

--- src/main/java/com/group4/expense_manager/config/AuthenticationEventListener.java
+++ src/main/java/com/group4/expense_manager/config/AuthenticationEventListener.java
@@ -5,6 +5,7 @@ import com.group4.expense_manager.entity.User;
 import com.group4.expense_manager.service.ActivityLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
@@ -16,18 +17,21 @@ public class AuthenticationEventListener {
 
     @EventListener
     public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
-        Object principal = event.getAuthentication().getPrincipal();
-        
-        if (principal instanceof User user) {
-            // Tạo activity log
-            ActivityLog log = new ActivityLog();
-            log.setUser(user);
-            log.setAction("LOGIN");
-            log.setTargetEntity("User");
-            log.setTargetId(user.getId());
-            log.setDescription("Đã đăng nhập");
-            
-            activityLogService.createActivityLog(log);
-        }
+        // if (event.getAuthentication() instanceof UsernamePasswordAuthenticationToken){
+        //     Object principal = event.getAuthentication().getPrincipal();
+
+        //     if (principal instanceof User user) {
+        //         // Tạo activity log
+        //         ActivityLog log = new ActivityLog();
+        //         log.setUser(user);
+        //         log.setAction("LOGIN");
+        //         log.setTargetEntity("User");
+        //         log.setTargetId(user.getId());
+        //         log.setDescription("Đã đăng nhập");
+
+        //         activityLogService.createActivityLog(log);
+        //     }
+        // }
+
     }
 }
--- src/main/java/com/group4/expense_manager/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/group4/expense_manager/config/CustomAuthenticationSuccessHandler.java
@@ -1,13 +1,18 @@
 package com.group4.expense_manager.config;
 
+import com.group4.expense_manager.entity.User;
+import com.group4.expense_manager.service.ActivityLogService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.group4.expense_manager.entity.ActivityLog;
 
 import java.io.IOException;
 import java.util.Set;
@@ -15,10 +20,22 @@ import java.util.Set;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Autowired
+    private ActivityLogService activityLogService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_ADMIN")) {
+            if (authentication.getPrincipal() instanceof User user) {
+                ActivityLog log = new ActivityLog();
+                log.setUser(user);
+                log.setAction("LOGIN");
+                log.setTargetEntity("User");
+//                log.setTargetId();
+                log.setDescription("Admin đăng nhập");
+                activityLogService.createActivityLog(log);
+            }
             response.sendRedirect("/admin/dashboard");
         }
         else {
